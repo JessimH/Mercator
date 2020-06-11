8c9e2acc9a3d345aba67a65491028fe573e3db54
JessimH <a121298@hotmail.fr>
2020-06-11 15:53:37 +0200

    Update cart.php

--- views/partials/cart.php
+++ views/partials/cart.php
@@ -1,5 +1,5 @@
 <section class="col cart">
-    <?php if($_SESSION['user'] && $_SESSION['user']['is_admin']==0) :?>
+    <?php if(isset($_SESSION['user']) && $_SESSION['user']['is_admin']==0) :?>
             <div class="col cart-title">
                 <h2>Joueurs sélectionné pour transfert</h2>
                 <hr>
@@ -19,10 +19,13 @@
             <div class="col cart-buy center">
                 <button class=" btn-buy ">Contactez les Agents</button>
             </div>
-    <?php else: ?>
+    <?php elseif(isset($_SESSION['user'])&& $_SESSION['user']['is_admin']==1) :?>
         <div class="row center cart-content">
-            <h2>Vous devez être engagé par un club pour pouvoir avoir accès à la lise des transferts</h2>
-            
+            <h2>En tant qu'Admin, vous n'avez pas accès a la liste des transferts</h2>      
+        </div>
+    <?php else:?>
+        <div class="row center cart-content">
+            <h2>Vous devez être engagé par un club pour pouvoir avoir accès à la lise des transferts</h2>      
         </div>
     <?php endif; ?>
 </section>
