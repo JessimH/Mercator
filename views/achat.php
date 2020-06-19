c0a67f31fee12709a3e6db20ae4cf23882cf6383
JessimH <a121298@hotmail.fr>
2020-06-19 23:17:53 +0200

    Update achat.php

--- views/achat.php
+++ views/achat.php
@@ -13,7 +13,11 @@
     <section class="col center index-content">
         <div class="col">
             <i style="font-size: 10em; color:#00B122; margin-bottom:20px;" class="fa fa-check-circle" aria-hidden="true"></i>
-            <h3>Nous avons bien pris en compte votre demande de transfert. <br> Le club séléctionné a été contacté. <br> Il donnera suite à votre demande par mail très prochainement.</h3>
+            <h3>Nous avons bien pris en compte votre demande de transfert. 
+                <br> 
+                Le club séléctionné a été contacté.
+                <br> 
+                Il donnera suite à votre demande par mail très prochainement.</h3>
         </div>
 
     </section>
