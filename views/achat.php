f6ddfb6597b0d1fe99b5e8ac41e10806c5821d62
JessimH <a121298@hotmail.fr>
2020-06-19 22:48:45 +0200

    orthograph

--- views/achat.php
+++ views/achat.php
@@ -11,7 +11,7 @@
    ?>
 
     <section class="col center index-content">
-        <h3>Les Clubs on été contacté, vous recevrez bientot par mail leur reponse</h3>
+        <h3>Nous avons bien prus en compte votre demande de transfert. Le club séléctionné a été contacté. Il donnera suite à votre demande par mail très prochainement.</h3>
         </div>
 
     </section>
